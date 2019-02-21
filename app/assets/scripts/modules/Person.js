8ab11b12f4389a4659525e1b636a6f8978aef50a
Michael <Reminisecence@Michaels-MacBook-Air-2.local>
2019-02-21 16:21:35 +1100

    Completed styling section

---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,7 @@
+function Person(fullName, favColor) {
+  this.name = fullName;
+  this.favoriteColor = favColor;
+  this.greet = function() {
+    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+  }
+}
